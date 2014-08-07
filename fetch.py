bc3c35ec275af8903cb7f3665b91292c0dcf213f
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-08-07 21:03:04 +0200

    Use urllib to reduce dependencies + output to script dir

--- fetch.py
+++ fetch.py
@@ -1,11 +1,11 @@
-import requests, datetime
+import urllib, datetime, os
 
 def fetch():
     url = 'http://www.bey2ollak.com/Traffic?action=getTraffic&target=4&city=0&lang=1'
-    content = requests.get(url).content
-    output_file_name = datetime.datetime.now().strftime('bey2ollak.%Y-%m-%d-%H-%M-%S.html')
-    open(output_file_name, 'w').write(content)
-    print output_file_name
+    output = datetime.datetime.now().strftime('bey2ollak.%Y-%m-%d-%H-%M-%S.html')
+    output = os.path.join(os.path.dirname(__file__), output)
+    urllib.urlretrieve(url, output)
+    print output
 
 if __name__ == '__main__':
     fetch()
